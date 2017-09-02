cb884b3f086a2f2704c373cf5881883dde83eda1
rwStarnight <29952469+rwStarnight@users.noreply.github.com>
2017-09-02 12:51:08 -0400

    first try

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+  var formattedName = HTMLheaderName.replace("%data%","rwStarNight");
+
+  var formattedRole = HTMLheaderRole.replace("%data%","web developer & app developer");
+
+  $("#header").prepend(formattedRole);
+
+  $("#header").prepend(formattedName);
